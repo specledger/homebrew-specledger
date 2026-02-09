e502f4c10cc96c38947a33f04eab43ed72362b47
Ariel Miki Abraham <arielabraham82@gmail.com>
2026-02-09 11:02:19 +0800

    Move formula to Formula/ folder
    Homebrew taps require formulas to be in the Formula/ subdirectory.

--- Formula/specledger.rb
+++ Formula/specledger.rb
@@ -1,32 +1,20 @@
-# Homebrew formula for SpecLedger CLI
 class Specledger < Formula
-  desc "All-in-one SDD Playbook for modern development teams"
-  homepage "https://specledger.io"
-  url "https://github.com/specledger/specledger/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "da39a3ee5e6b4b0d3255bfef95601890afd80709" # Placeholder - will be updated by GoReleaser
+  desc "Unified CLI for project bootstrap and specification dependency management"
+  homepage "https://github.com/specledger/specledger"
+  url "https://github.com/specledger/specledger/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "c22f1babb18499de478d52c38e6ec3e1bb92618e5366fe79a257a5ee35ed7ab7"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/specledger/specledger/releases/download/v1.0.0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
-    sha256 cellar: :any_skip_relocation, sequoia:         "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
-    sha256 cellar: :any_skip_relocation, sonoma:          "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
-    sha256 cellar: :any_skip_relocation, ventura:         "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
-    sha256 cellar: :any_skip_relocation, monterey:        "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
-    sha256 cellar: :any_skip_relocation, x86_64_linux:    "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder
-  end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(output: bin/"sl", ldflags: "-s -w"), "./cmd"
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/sl"
   end
 
   test do
-    system bin/"sl", "--version"
-    assert_match(/specledger/i, shell_output("#{bin}/sl --help"))
+    system bin/"sl", "version"
   end
+
+  # Disable bottle generation - build from source only
+  bottle :unneeded
 end
