1f455e3235e8b4596971edcb067bf99d91dc8042
Ariel Miki Abraham <arielabraham82@gmail.com>
2026-02-09 10:49:01 +0800

    Update to v1.0.4

--- specledger.rb
+++ specledger.rb
@@ -1,8 +1,8 @@
 class Specledger < Formula
   desc "Unified CLI for project bootstrap and specification dependency management"
   homepage "https://github.com/specledger/specledger"
-  url "https://github.com/specledger/specledger/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "f2162d684093e95ec3fa21e7df10945585e3226e45ade6d1048db00752bc98f2"
+  url "https://github.com/specledger/specledger/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "60b92240535ee65ac90e24a9b581289602eeda8172e28cd6efc3bfa4f05e049a"
   license "MIT"
 
   depends_on "go" => :build
