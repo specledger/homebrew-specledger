a89ea97a069216f97ad4c45fd198c2dbbe6dba89
Ariel Miki Abraham <arielabraham82@gmail.com>
2026-02-09 10:58:49 +0800

    Update to v1.0.5

--- specledger.rb
+++ specledger.rb
@@ -1,14 +1,14 @@
 class Specledger < Formula
   desc "Unified CLI for project bootstrap and specification dependency management"
   homepage "https://github.com/specledger/specledger"
-  url "https://github.com/specledger/specledger/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "60b92240535ee65ac90e24a9b581289602eeda8172e28cd6efc3bfa4f05e049a"
+  url "https://github.com/specledger/specledger/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "c22f1babb18499de478d52c38e6ec3e1bb92618e5366fe79a257a5ee35ed7ab7"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd"
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/sl"
   end
 
   test do
